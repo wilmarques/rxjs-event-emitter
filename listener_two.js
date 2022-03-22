ca5da638eaf8722dc2cda9cac6a9286eea756a52
wilmarques <wileymarques@live.com>
2022-03-22 01:59:22 +0000

    Removing Listener 2 subscription after 5 seconds

--- listener_two.js
+++ listener_two.js
@@ -1,11 +1,21 @@
-import { fromEvent } from 'https://cdn.skypack.dev/rxjs';
-import { tap } from 'https://cdn.skypack.dev/rxjs/operators';
+import { fromEvent, interval } from 'https://cdn.skypack.dev/rxjs';
+import { take, tap } from 'https://cdn.skypack.dev/rxjs/operators';
 
-fromEvent(window, 'request')
+import { log } from './log.js';
+
+const subscription = fromEvent(window, 'request')
   .pipe(
     tap(({ detail }) => {
       const { resolve } = detail;
       resolve('Listener 2');
     }),
+  )
+  .subscribe();
+
+interval(5000)
+  .pipe(
+    take(1),
+    tap(() => subscription.unsubscribe()),
+    tap(() => log('Listener 2 - Unsubscribed')),
   )
   .subscribe();
