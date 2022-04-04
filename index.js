290b0f9c5086828caeb0655964f6a62992335b0b
wilmarques <wileymarques@live.com>
2022-04-04 21:17:34 +0000

    WIP

--- index.js
+++ index.js
@@ -1,13 +1,57 @@
-import { fromEvent } from 'https://cdn.skypack.dev/rxjs';
-import { tap } from 'https://cdn.skypack.dev/rxjs/operators';
+//@ts-check
 
-import { emit } from './emitter.js';
+import { fromEvent, merge, interval } from 'https://cdn.skypack.dev/rxjs';
+import { bufferTime, takeUntil, skipWhile, delay } from 'https://cdn.skypack.dev/rxjs/operators';
 
-import './listener_one.js';
-import './listener_two.js';
+import { log } from './log.js';
 
-fromEvent(document, 'click')
+const EVENTS = [
+  fromEvent(document, 'click'),
+  fromEvent(document, 'mousedown'),
+  fromEvent(document, 'keypress'),
+  fromEvent(document, 'DOMMouseScroll'),
+  fromEvent(document, 'mousewheel'),
+  fromEvent(document, 'touchmove'),
+  fromEvent(window, 'mousemove'),
+  fromEvent(window, 'resize'),
+];
+
+/**
+ * @type number
+ */
+const refreshTimerInMs = 1000;
+
+/**
+ * @type number
+ */
+const maxInactivityInMs = 5000;
+
+const inactivity$ = merge(...EVENTS)
   .pipe(
-    tap(() => emit()),
-  )
-  .subscribe();
+    bufferTime(maxInactivityInMs),
+    skipWhile((events) => events.length > 0),
+  );
+
+/**
+ * @param {number} policyCont
+ */
+const refresh = (policyCont) => {
+  log(`Refreshed - Policy ${policyCont}`);
+  initializeRefreshPolicy();
+};
+const stop = (policyCont) => log(`Stopped - Policy ${policyCont}`);
+
+let cont = 0;
+const initializeRefreshPolicy = () => {
+  const policyCont = cont++;
+  interval(refreshTimerInMs)
+    .pipe(
+      takeUntil(inactivity$),
+    )
+    .subscribe({
+      next: () => refresh(policyCont),
+      complete: () => stop(policyCont),
+    });
+};
+
+initializeRefreshPolicy();
